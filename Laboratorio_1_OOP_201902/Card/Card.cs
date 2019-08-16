c5d60a2af5b23bde54e55cbd78a6d6c00e17ade1
Franciscob95 <fjbarros1@miuandes.cl>
2019-08-16 09:56:15 -0400

    first commit

---
+++ Laboratorio_1_OOP_201902/Card/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_1_OOP_201902.Card
+{
+    public class Card
+    {
+        protected string name,type,effect;
+        public Card(string name, string type, string effect)
+        {
+            this.name = name;
+            this.type = type;
+            this.effect = effect;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return this.type;
+            }
+            set
+            {
+                this.type = value;
+            }
+        }
+        public string Effect
+        {
+            get
+            {
+                return this.effect;
+            }
+            set
+            {
+                this.effect = value;
+            }
+        }
+    }
+}
---
+++ Laboratorio_1_OOP_201902/Deck.cs
@@ -0,0 +1,31 @@
+﻿using Laboratorio_1_OOP_201902.Card;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_1_OOP_201902
+{
+    public class Deck
+    {
+        protected List<CombatCard> combatCards;
+        protected List<SpecialCard> SpecialCards;
+
+        public Deck(List<CombatCard> combatCards, List<SpecialCard> SpecialCards)
+        {
+            this.combatCards = combatCards;
+            this.SpecialCards = SpecialCards;
+        }
+ 
+        public List<CombatCard> CombatCards { get => combatCards; set => combatCards = value; }
+        public List<SpecialCard> SpecialCards1 { get => SpecialCards; set => SpecialCards = value; }
+
+        public void AddCombatCard(CombatCard combatCard) { throw new NotImplementedException(); }
+        public void AddSpecialCard(SpecialCard specialCard) { throw new NotImplementedException(); }
+        public void DestroyCombatCard(int cardId) { throw new NotImplementedException(); }
+        public void DestroySpecialCard(int cardId) { throw new NotImplementedException(); }
+        public void Shuffle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
